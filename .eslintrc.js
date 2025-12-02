e001d36127632766c924098f89d1e47deee6bcf9
AlexInstituto <aangbla317@g.educaand.es>
2025-12-02 09:58:25 +0100

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+
+  rules: {
+    'no-unused-vars': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}],
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+};
